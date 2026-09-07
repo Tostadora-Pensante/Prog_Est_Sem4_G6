a877096d0e8cdca67278ecd66ee34e270771d0a5
Tostadora-Pensante <jdmullerc@uamv.edu.ni>
2026-09-07 13:40:31 -0600

    Actualizo los Archivos

--- calculadora/principal.py
+++ calculadora/principal.py
@@ -1,3 +1,6 @@
 import aritmetica as arit
 
 print(f"la suma es: {arit.add(2,2)}")
+print(f"la resta es: {arit.substract(4,10)}")
+print(f"la multiplicacion es: {arit.mult(5,7)}")
+print(f"la division es: {arit.div(4,2)}")
