47b522a3ab8574d456a1d17bcc10ac8f3eeea093
Tostadora-Pensante <jdmullerc@uamv.edu.ni>
2026-09-09 10:17:37 -0600

    Agrego los Archivos

---
+++ miercoles/notas.py
@@ -0,0 +1,22 @@
+from colorama import Fore
+def amountGrades():
+    grades = int(input("Ingrese la cantidad de notas a evaluar"))
+    return grades
+
+def evaluationGrade():
+    if grade < 60:
+        print("Esta nota es de aprendizaje inicial, se recomienda estudiar :U")
+                 
+    elif grade = 60 and grade <= 75:
+        return "esta nota es de aprendizaje fundamental, aceptable :v"
+
+    elif grade >= 76 and grade <= 89:
+        return "Esta nota es de aprendizaje Satisfactoria, en hora buena :D" 
+    else grade > 90 and grade <= 100 :
+        return "Esta nota es de aprendizaje Avanzado! Buena nota :P"
+
+
+
+
+
+                 
