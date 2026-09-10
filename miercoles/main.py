2d3add64292ec1340eff04215b1d22206c7dec52
Tostadora-Pensante <jdmullerc@uamv.edu.ni>
2026-09-09 21:05:43 -0600

    Actualizo los Archivos

---
+++ miercoles/main.py
@@ -0,0 +1,32 @@
+import notas 
+from colorama import Fore
+def greet():
+    print(Fore.CYAN + "Bienvenidos a este Programa Evaluador hecho en 10 minutos :v", Fore.RESET)
+    print(Fore.BLUE + "Este sistema no te califica como tal, solo evalua to nota y te dice como clasifica segun el sistma educativo del minet.", Fore.RESET)
+    print(Fore.RED + "Recuerden una mala nota no los define, pero si estudien a no ser que quieran botar la carrera!", Fore.RESET)
+
+def menu():
+    print("Evaluador Insano:")
+    print("1. Evaluar notas")
+    print("2. Explicacion de que Significa cada categoria.")
+    print("0. Salir del programa.")
+    op = int(input("Digite el # de la opcion que desea usar."))
+    return op
+
+
+def chooseOp(op):
+    if op == 1:
+        nota = notas.readValue()
+        notas.evaluationGrade(nota)
+    elif op == 2:
+        notas.explanation()
+    elif op == 0:
+        print("Gracias por usar mi calculadora :D")
+        
+def main():
+    while True:
+            op = menu()
+            if op > 0 and op <=2: chooseOp(op)
+            elif op ==(0): break
+            else: print("Opcion no valida, intente de nuevo")  
+
--- miercoles/notas.py
+++ miercoles/notas.py
@@ -1,19 +1,35 @@
 from colorama import Fore
-def amountGrades():
+def amountGrades(grades):
     grades = int(input("Ingrese la cantidad de notas a evaluar"))
     return grades
 
-def evaluationGrade():
+def readValue():
+    grade = float(input("Digite la nota a evaluar porfavor: "))
+    return grade
+
+
+def evaluationGrade(grade):
     if grade < 60:
-        print("Esta nota es de aprendizaje inicial, se recomienda estudiar :U")
+       print(Fore.RED + "Esta nota es de aprendizaje inicial, se recomienda estudiar :U", Fore.RESET)
                  
-    elif grade = 60 and grade <= 75:
-        return "esta nota es de aprendizaje fundamental, aceptable :v"
+    elif grade == 60 and grade <= 75:
+         print(Fore.YELLOW +"esta nota es de aprendizaje fundamental, aceptable :v", Fore.RESET)
 
     elif grade >= 76 and grade <= 89:
-        return "Esta nota es de aprendizaje Satisfactoria, en hora buena :D" 
-    else grade > 90 and grade <= 100 :
-        return "Esta nota es de aprendizaje Avanzado! Buena nota :P"
+         print(Fore.LIGHTGREEN_EX + "Esta nota es de aprendizaje Satisfactoria, en hora buena :D", Fore.RESET) 
+
+    elif grade > 90 and grade <= 100:
+        print(Fore.GREEN + "Esta nota es de aprendizaje Avanzado! Buena nota :P", Fore.RESET)
+
+
+
+def explanation():
+    print("Aprendizaje Inicial se refiere a cualquier nota por debajo de 60, normalmente es una forma suave de decir que el estudiante reprobo.")
+    print("Aprendizaje Fundamental se refiere a que el estudiante paso con el minimo necesario para aprobar la evaluación.")
+    print("Aprendizaje Satisfactorio significa que el estudiante paso de forma buena la evaluación.")
+    print("Aprendizaje Avanzado significa que el estudiante aprobo de forma más que satisfactoria la evaluación.")
+
+
 
 
 
